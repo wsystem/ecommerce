e88dd4811ba692fb6bae0935429d6ad5f035c607
wsystem <wsysandsol@gmail.com>
2018-09-13 22:13:14 -0300

    13/09/2018

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+
+	require_once("vendor/autoload.php");
+
+	$app = new \Slim\Slim();
+
+	$app->config('debug', true);
+
+	$app->get('/', function() {
+
+		echo "Bem-Vindo ao Ecommerce!";
+	});
+
+	$app->run();
+
+ ?>
